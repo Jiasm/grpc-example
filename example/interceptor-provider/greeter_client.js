63d77895483a1d3a7bb139b5c7e39f1f4b06c22c
jarvis <jiashunming@outlook.com>
2020-11-27 10:13:41 +0800

    build: add examples

---
+++ example/interceptor-provider/greeter_client.js
@@ -0,0 +1,54 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const messages = require('../../helloworld_pb');
+const services = require('../../helloworld_grpc_pb');
+
+const grpc = require('grpc');
+const { InterceptingCall } = grpc;
+
+function main() {
+  var target = 'localhost:50051';
+  var interceptor = function(options, nextCall) {
+    console.log('trigger')
+    return new InterceptingCall(nextCall(options));
+  }
+
+  const interceptorProvider = (methodDefinition) => {
+    console.log('call interceptorProvider', methodDefinition)
+    return interceptor
+  }
+
+  var client = new services.GreeterClient(
+    target,
+    grpc.credentials.createInsecure(),
+    {
+      interceptor_providers: [interceptorProvider]  
+    }
+  );
+  var request = new messages.HelloRequest();
+  request.setName('world');
+  client.sayHello(request, function(err, response) {
+    console.log('Greeting:', response.getMessage());
+  });
+  client.sayHello(request, function(err, response) {
+    console.log('Greeting:', response.getMessage());
+  });
+}
+
+main();
